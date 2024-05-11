35066b8e24df53f46455b4480bff42f6b7be9c85
GingerGenie <GingerGenie@proton.me>
2024-05-11 09:19:41 +0300

    Fix some problems

--- script.js
+++ script.js
@@ -88,25 +88,34 @@ const profileMenu = document.querySelector('.navigation__profile-menu');
 let profileFl = false;
 
 profileButton.addEventListener('click', () => {
+    attention.remove(); // удаляет заметку о рабочей кнопке
+}, {once: true})
+
+profileButton.addEventListener('click', () => {
     if (!profileFl) {
         profileMenu.classList.add('display-view')
         profileFl = true;
+        profileButton.classList.add('active-link-profile');
     }
     else {
         profileMenu.classList.remove('display-view')
         profileFl = false;
+        profileButton.classList.remove('active-link-profile');
     }
 })
 
+// Меню бургер для мобилок
 const burgerButton = document.querySelector('.mobile-navigation__info__burger-menu');
 const burgerMenu = document.querySelector('.wrap-for-mobile-burger');
+const yandexMap = document.getElementById('yandexmap'); // карта исчезает т.к. есть коллизии у меню и картой
 let burgerFl = false;
 
 burgerButton.addEventListener('click', () => {
-    
+    console.log(yandexMap)
     if (!burgerFl) {
         burgerButton.setAttribute('disabled', true);
-        burgerMenu.style.display = 'flex'; // flex
+        yandexMap.style.display = 'none';
+        burgerMenu.style.display = 'flex';
         burgerMenu.style.animationName = 'burger-animation';
         burgerFl = true;
         setTimeout(() => burgerButton.removeAttribute("disabled"), 1000);
@@ -115,6 +124,7 @@ burgerButton.addEventListener('click', () => {
         burgerButton.setAttribute('disabled', true);
         burgerMenu.style.animationName = 'burger-animation-reverse';
         setTimeout(() => {
+            yandexMap.style.display = 'block';
             burgerMenu.style.display = 'none';
             burgerButton.removeAttribute("disabled");
         }, 900)
