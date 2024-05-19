e4ef9ca9da1ecec1f4954839e2101ac2dd3b2f1f
Aidar <157820661+GingerGenie@users.noreply.github.com>
2024-05-19 17:31:59 +0300

    Update initialization.js

--- initialization.js
+++ initialization.js
@@ -2,49 +2,37 @@ import objPages from './js/pages.js';
 
 const root = document.getElementById('root');
 
-addEventListener('DOMContentLoaded', () => {
+function initPage(dispatch) {
     let fragment = window.location.href;
+    const regExp = new RegExp('[\/](?![:0-9A-Za-z#_.-]*[\/])');
+
     if (fragment[fragment.length-1] === '/') fragment = fragment.slice(0, -1);
     fragment = fragment.slice(
-        fragment.search(/[\/](?![:0-9A-Za-z#_.-]*[\/])/) === -1 ? 
-            fragment.search(/[\/](?![:0-9A-Za-z#_.-]*[\/])/) : 
-            fragment.search(/[\/](?![:0-9A-Za-z#_.-]*[\/])/)+1
+        fragment.search(regExp) === -1 ? 
+            fragment.search(regExp) : 
+            fragment.search(regExp)+1
         , fragment.length);
 
     if (fragment === '' || fragment == "WebBee-first-task") {
-        fragment = 'activity';
-        window.location = 'https://gingergenie.github.io/WebBee-first-task' + '/activity';
-        root.innerHTML = objPages[fragment]['html'];
+        switch (dispatch) {
+            case 'openPage':
+                fragment = 'activity';
+                window.location = 'https://gingergenie.github.io/WebBee-first-task' + '/activity';
+                root.innerHTML = objPages[fragment]['html'];
+                break;
+            case 'popstate':
+                history.back();
+                break;
+        }
     }
-    else {
-        objPages[fragment]['state']['init']();
-    }
-
-    const needButton = document.querySelector(`[href="/${fragment}"]`);
-    needButton.classList.add('active-link');
-    objPages.disabledButton = needButton;
-})
-
-
-addEventListener('popstate', () => {
-    let fragment = window.location.href;
-    if (fragment[fragment.length-1] === '/') fragment = fragment.slice(0, -1);
-    fragment = fragment.slice(
-        fragment.search(/[\/](?![:0-9A-Za-z#_.-]*[\/])/) === -1 ? 
-            fragment.search(/[\/](?![:0-9A-Za-z#_.-]*[\/])/) : 
-            fragment.search(/[\/](?![:0-9A-Za-z#_.-]*[\/])/)+1
-        , fragment.length);
-
-    if (fragment === '' || fragment == "127.0.0.1:5500") {
-        history.back();
-    }
-    else {
-        root.innerHTML = objPages[fragment]['html'];
-        objPages[fragment]['state']['init']();
-    }
+    if (dispatch === 'popstate') root.innerHTML = objPages[fragment]['html']
+    objPages[fragment]['state']['init']();
 
     const needButton = document.querySelector(`[href="/${fragment}"]`);
     objPages.disabledButton?.classList.remove('active-link');
     needButton.classList.add('active-link');
     objPages.disabledButton = needButton;
-})
+}
+
+addEventListener('DOMContentLoaded', initPage.call(null, 'openPage'))
+addEventListener('popstate', initPage.call(null, 'popstate'))
